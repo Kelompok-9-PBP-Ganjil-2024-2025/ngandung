7cf80f391c8448d8c8a8ef54c20a9ffc8beffa99
arshqiii <marshq23@gmail.com>
2024-10-22 11:32:05 +0700

    fixed misplaced models & updated Installed apps in settings

--- ngandung/settings.py
+++ ngandung/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'livereload'
+    'livereload',
+    'tambah_toko_makanan',
 ]
 
 MIDDLEWARE = [
