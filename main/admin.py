2ba12988a72f85a836abcc9d3f149bc4b1a1b0fe
RayhanSP <ray.han2005.rh@gmail.com>
2024-10-23 16:37:23 +0700

    added tambah_favorit in views.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import UserProfile, Toko, Makanan
+
+# Register the models so they are accessible in the admin interface
+admin.site.register(UserProfile)
+admin.site.register(Toko)
+admin.site.register(Makanan)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from main.models import Makanan, Toko, UserProfile
+from django.shortcuts import get_object_or_404, redirect
 
 
 # Create your views here.
@@ -8,3 +10,9 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def tambah_ke_favorit(request, toko_id):
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    toko = get_object_or_404(Toko, id=toko_id)
+    user_profile.favorit_toko.add(toko)
+    return redirect('daftar_favorit')
