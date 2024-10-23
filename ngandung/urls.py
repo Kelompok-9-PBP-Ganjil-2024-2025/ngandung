19c425bd84b8223ae405a31e9fbd85b9675bb8dc
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-10-23 16:31:13 +0700

    Handling URL not found

--- ngandung/urls.py
+++ ngandung/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
+    # To handle any URL that doesn't match the ones above
+    re_path(r"^.*$", RedirectView.as_view(url="/", permanent=False)),
 ]
