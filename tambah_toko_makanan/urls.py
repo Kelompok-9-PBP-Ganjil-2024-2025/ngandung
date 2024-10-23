7f6f58bae6daa38e526ab4c2ed3eb7ad273f85ef
arshqiii <marshq23@gmail.com>
2024-10-23 16:37:38 +0700

    routed view functions in urls.py app

---
+++ tambah_toko_makanan/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from tambah_toko_makanan.views import *
+
+app_name = 'tambah_toko_makanan'
+
+urlpatterns = [
+    path('add-toko', create_toko, name='create_toko'),
+    path('edit-toko/<uuid:id>', edit_toko, name='edit_toko'),
+    path('detail-toko/<uuid:id>', detail_toko, name='detail_toko'),
+    path('delete-toko/<uuid:id>', delete_toko, name='delete_toko'),
+    path('add-makanan', create_makanan, name='create_toko'),
+    path('edit-makanan/<uuid:id>', edit_makanan, name='edit_toko'),
+    path('detail-makanan/<uuid:id>', detail_makanan, name='detail_toko'),
+    path('delete-makanan/<uuid:id>', delete_makanan, name='delete_makanan'),
+]
