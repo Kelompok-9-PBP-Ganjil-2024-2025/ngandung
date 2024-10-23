ab103d2268c6704633536740ce370419876c374f
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2024-10-23 16:56:22 +0700

    Checkpoint discussion forum

---
+++ authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from authentication.views import show_main
+from authentication.views import login_user
+from authentication.views import register
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    
+    path('login/', login_user, name='login'),
+    path('register/', register, name='register'),
+]
--- food_discussion_forum/models.py
+++ food_discussion_forum/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Discussion(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by (self.user) on (self.discussion.title)"
