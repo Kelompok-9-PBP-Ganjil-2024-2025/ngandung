42851cf2376cbf9070a7ab468a69bee01f7a860c
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2024-10-24 18:38:24 +0700

    test: mau ngepull main tapi harus push duluu

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from authentication.views import show_main
 from authentication.views import login_user
 from authentication.views import register
 from authentication.views import logout_user
+
 app_name = "authentication"
 
 urlpatterns = [
--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
