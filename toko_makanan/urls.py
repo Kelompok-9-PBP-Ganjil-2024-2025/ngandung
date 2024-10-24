44e0a252788770b3b320c8645fcb9e9d419a8dfa
arshqiii <marshq23@gmail.com>
2024-10-24 09:22:48 +0700

    updated templates and urls routing

--- toko_makanan/urls.py
+++ toko_makanan/urls.py
@@ -5,12 +5,12 @@ app_name = 'toko_makanan'
 
 urlpatterns = [
     path("", show_main, name="show_main"),
-    path('add-toko', create_toko, name='create_toko'),
-    path('edit-toko/<uuid:id>', edit_toko, name='edit_toko'),
-    path('detail-toko/<uuid:id>', detail_toko, name='detail_toko'),
-    path('delete-toko/<uuid:id>', delete_toko, name='delete_toko'),
-    path('add-makanan', create_makanan, name='create_toko'),
-    path('edit-makanan/<uuid:id>', edit_makanan, name='edit_toko'),
-    path('detail-makanan/<uuid:id>', detail_makanan, name='detail_toko'),
-    path('delete-makanan/<uuid:id>', delete_makanan, name='delete_makanan'),
+    path('add-toko/', create_toko, name='add_toko'),
+    path('edit-toko/<uuid:id>/', edit_toko, name='edit_toko'),
+    path('detail-toko/<uuid:id>/', detail_toko, name='detail_toko'),
+    path('delete-toko/<uuid:id>/', delete_toko, name='delete_toko'),
+    path('add-makanan/', create_makanan, name='add_makanan'),
+    path('edit-makanan/<uuid:id>/', edit_makanan, name='edit_makanan'),
+    path('detail-makanan/<uuid:id>/', detail_makanan, name='detail_makanan'),
+    path('delete-makanan/<uuid:id>/', delete_makanan, name='delete_makanan'),
 ]
