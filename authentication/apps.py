6fa2c7c0408e30913b92bfd62d51ce16f9be9e8b
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-10-25 19:51:50 +0700

    ADMIN PRE-POPULATE IS IMPLEMENTED

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.contrib.auth import get_user_model
+
+def create_admin(sender, **kwargs):
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(username='admin', password='admin123', is_superuser=True)
+        print("=========\nAdmin user created\n=========")
 
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+    def ready(self):
+        post_migrate.connect(create_admin, sender=self)
