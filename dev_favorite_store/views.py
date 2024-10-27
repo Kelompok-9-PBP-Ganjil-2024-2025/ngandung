98173326ebaac25260e1ab58a9106a9562d4a7fd
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-10-28 00:13:45 +0700

    Fixing conflict

--- dev_favorite_store/views.py
+++ dev_favorite_store/views.py
@@ -8,9 +8,6 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import JsonResponse
-
-
- 
 from django.http import JsonResponse, HttpResponseBadRequest
 
 
