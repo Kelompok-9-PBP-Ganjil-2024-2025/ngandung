88732e42746fbe67010f408251b279c2130856a4
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-10-27 22:57:02 +0700

    Updated urls and added tests

--- toko_makanan/tests.py
+++ toko_makanan/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from toko_makanan.models import RumahMakan, Makanan
 
-# Create your tests here.
+
+class RumahMakanMakananTests(TestCase):
+    def setUp(self):
+        # Setup rumah makan
+        self.rumah_makan = RumahMakan.objects.create(
+            kode_provinsi=31,
+            nama_provinsi="DKI Jakarta",
+            bps_kode_kabupaten_kota=3171,
+            bps_nama_kabupaten_kota="Jakarta Selatan",
+            nama_rumah_makan="Dallas",
+            alamat="Jalan Merdeka No.1",
+            latitude=-6.21462,
+            longitude=106.84513,
+            tahun=2020,
+            masakan_dari_mana="Jawa",
+            makanan_berat_ringan="Berat",
+        )
+
+        # Setup makanan
+        self.makanan = Makanan.objects.create(
+            name="Nasi Kimpul", price=10000, rumah_makan=self.rumah_makan
+        )
+        self.client = Client()
+
+    def test_rumah_makan_creation(self):
+        self.assertEqual(self.rumah_makan.nama_rumah_makan, "Dallas")
+        self.assertEqual(self.rumah_makan.nama_provinsi, "DKI Jakarta")
+
+    def test_show_main_view(self):
+        response = self.client.get(reverse("toko_makanan:show_main"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "mainPage/index.html")
+        self.assertContains(response, self.rumah_makan.nama_rumah_makan)
+
+    def test_rumah_makan_by_id_exist(self):
+        response = Client().get(
+            reverse("toko_makanan:detail_rumah_makan", args=[self.rumah_makan.pk])
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_rumah_makan_by_id_without_login(self):
+        response = Client().get(
+            reverse("toko_makanan:edit_toko", args=[self.rumah_makan.pk])
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_rumah_makan_by_id_without_login(self):
+        response = Client().get(
+            reverse("toko_makanan:delete_toko", args=[self.rumah_makan.pk])
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_makanan_creation(self):
+        self.assertEqual(self.makanan.name, "Nasi Kimpul")
+        self.assertEqual(self.makanan.price, 10000)
+        self.assertEqual(self.makanan.rumah_makan, self.rumah_makan)
+
+    def test_add_makanan_without_login(self):
+        response = self.client.post(
+            reverse("toko_makanan:add_makanan"),
+            {"name": "Bakso", "price": 12000, "toko_id": self.rumah_makan.id},
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_makanan_by_id_exist_without_login(self):
+        response = Client().get(
+            reverse("toko_makanan:edit_makanan", args=[self.makanan.pk])
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_makanan_by_id_exist_without_login(self):
+        response = Client().get(
+            reverse("toko_makanan:delete_makanan", args=[self.makanan.pk])
+        )
+        self.assertEqual(response.status_code, 302)
--- toko_makanan/urls.py
+++ toko_makanan/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('delete-toko/<int:id>/', delete_toko, name='delete_toko'),
     path('add-makanan/', add_makanan_ajax, name='add_makanan'),
     path('edit-makanan/<int:id>/', edit_makanan, name='edit_makanan'),
-    path('detail-rumah-makan/<int:id>/', detail_rumah_makan, name='detail_makanan'),
+    path('detail-rumah-makan/<int:id>/', detail_rumah_makan, name='detail_rumah_makan'),
     path('delete-makanan/<int:id>/', delete_makanan, name='delete_makanan'),
     path('makanan-json/', makanan_json, name='makanan_json'),
 ]
