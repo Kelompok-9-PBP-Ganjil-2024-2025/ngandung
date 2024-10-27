88e485abd8e7b874de2bd39277ae6a07a5691731
RayhanSP <ray.han2005.rh@gmail.com>
2024-10-27 23:06:18 +0700

    403 forb

--- dev_favorite_store/urls.py
+++ dev_favorite_store/urls.py
@@ -5,7 +5,7 @@ app_name = 'dev_favorite_store'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('tambah-ke-favorit/<int:toko_id>/', tambah_ke_favorit, name='tambah_ke_favorit'),
-    path('hapus-dari-favorit/<int:toko_id>/', hapus_dari_favorit, name='hapus_dari_favorit'),
+    path('tambah-ke-favorit/<int:id>', tambah_ke_favorite, name='tambah_ke_favorit'),
+    path('hapus-dari-favorit/<int:id>/', hapus_dari_favorit, name='hapus_dari_favorit'),
     path('daftar-favorit/', daftar_favorit, name='daftar_favorit'),
 ]
