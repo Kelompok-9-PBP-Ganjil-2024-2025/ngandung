1a4da693b309a0b851a06df9db9f9892585034e4
RayhanSP <ray.han2005.rh@gmail.com>
2024-10-27 12:28:16 +0700

    Daftar favorit: delete button now has AJAX

--- dev_favorite_store/models.py
+++ dev_favorite_store/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from main.models import RumahMakan
-
-# class Toko(models.Model):
-#     nama = models.CharField(max_length=100)
-#     alamat = models.CharField(max_length=255)
-#     telpon = models.CharField(max_length=20)
-
-#     def __str__(self) -> str:
-#         return self.nama
-    
-# class Makanan(models.Model): #Nanti class ini diapus, baru import class dari folder tambah makanan
-#     nama = models.CharField(max_length=100)
-#     harga = models.DecimalField(max_digits=8, decimal_places=2)
-#     toko = models.ForeignKey(Toko, on_delete=models.CASCADE)
-#     description = models.TextField(null=True, blank=True)
-
-#     def __str__(self):
-#         return self.nama
     
 class UserProfile(models.Model): #Ini juga
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- dev_favorite_store/views.py
+++ dev_favorite_store/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import UserProfile
+from django.http import JsonResponse, HttpResponseBadRequest
 
 
 # Create your views here.
@@ -21,11 +22,18 @@ def tambah_ke_favorit(request, toko_id):
     user_profile.favorit_toko.add(toko)
     return redirect('dev_favorite_store:daftar_favorit')
 
+@login_required
 def hapus_dari_favorit(request, toko_id):
-    user_profile = get_object_or_404(UserProfile, user=request.user)
-    toko = get_object_or_404(RumahMakan, id=toko_id)
-    user_profile.favorit_toko.remove(toko)
-    return redirect('dev_favorite_store:daftar_favorit')
+    if request.method == 'POST':
+        try:
+            user_profile = get_object_or_404(UserProfile, user=request.user)
+            toko = get_object_or_404(RumahMakan, id=toko_id)
+            user_profile.favorit_toko.remove(toko)
+            return JsonResponse({'status': 'success', 'message': 'Toko berhasil dihapus dari favorit.'})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+    else:
+        return HttpResponseBadRequest('Invalid request method.')
 
 @login_required(login_url='/login/')
 def daftar_favorit(request):
