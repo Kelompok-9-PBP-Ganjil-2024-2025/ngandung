eac67aa4341e8655b9c06660e88e1d50a0256e08
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-10-27 21:51:04 +0700

    Added whitenoise

--- ngandung/settings.py
+++ ngandung/settings.py
@@ -49,13 +49,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "livereload.middleware.LiveReloadScript",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'django.middleware.csrf.CsrfViewMiddleware',
+    "django.middleware.csrf.CsrfViewMiddleware",
 ]
 
 ROOT_URLCONF = "ngandung.urls"
