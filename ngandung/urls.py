14f9d98bff5b2d60b8f2c4e11d29c0d77bb12a64
arshqiii <marshq23@gmail.com>
2024-12-19 21:16:07 +0700

    made change to auth url

--- ngandung/urls.py
+++ ngandung/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", include("main.urls")),
     path("", include("toko_makanan.urls")),
     path("", include("authentication.urls")),
-    path("", include("auth_flutter.urls")),
+    path("auth/", include("auth_flutter.urls")),
     path("", include("rating_toko.urls")),
     path("", include("dev_favorite_store.urls")),
     path("", include("discuss_forum.urls")),
